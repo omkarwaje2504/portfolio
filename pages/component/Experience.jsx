924c73c0828486bb8d9a656d41f4693e712a5b4e
Omkar Waje <omkar@saiashirwad.com>
2023-06-01 19:54:23 +0530

    minor change

--- pages/component/Experience.jsx
+++ pages/component/Experience.jsx
@@ -4,15 +4,15 @@ const Experience = (props) => {
     return (
         <div>
             <div className='flex flex-col space-y-2'>
-                <p className='text-xl'>{props.year}</p>
-                <div className='flex flex-row w-full justify-between items-center'>
-                    <h1 className='2xl:text-4xl font-semibold uppercase text-[#4C89F8]'>{props.job}</h1>
-                    <p className='font-semibold text-3xl capitalize'>
+                <p className='text-lg md:text-xl'>{props.year}</p>
+                <div className='flex flex-col md:flex-row w-full justify-between md:items-center'>
+                    <h1 className='2xl:text-4xl text-2xl font-semibold uppercase text-[#4C89F8]'>{props.job}</h1>
+                    <p className='font-semibold text-lg md:text-3xl capitalize'>
                         {props.period}
                     </p>
                 </div>
-                <h1 className='text-2xl'>{props.company}</h1>
-                <h1 className='text-2xl pt-6 font-light'>{props.des}</h1>
+                <h1 className='text-lg md:text-2xl'>{props.company}</h1>
+                <h1 className='text-md md:text-2xl pt-3 md:pt-6 font-light'>{props.des}</h1>
             </div>
         </div>
     )
--- pages/component/Information.jsx
+++ pages/component/Information.jsx
@@ -17,12 +17,12 @@ const Information = () => {
                 </div>
                 <div className='md:mt-28 mt-8' data-aos="fade-up" data-aos-duration="700">
                     <div className='space-y-3'>
-                        <p className='lg:text-4xl text-md font-light lg:w-2/3'>Designing digital experiences that captivate and inspire.
+                        <p className='lg:text-4xl text-lg font-light lg:w-2/3'>Designing digital experiences that captivate and inspire.
                         </p>
                         <h1 className='md:text-6xl text-4xl font-normal'>welcome to my <b>portfolio!</b></h1>
                     </div>
                     <div className='mt-10 md:mt-20'>
-                        <p className='md:w-2/3 md:text-xl text-sm font-light'>I am <span className='text-[#4C89F8]'>omkar chandrakant waje</span>.<br></br>
+                        <p className='md:w-2/3 md:text-xl text-lg font-light'>I am <span className='text-[#4C89F8]'>omkar chandrakant waje</span>.<br></br>
                             Who aim to be able to find an exciting and challenging entre level
                             position in the it and graphic design sector alongside a company
                             who will continously motivate and improve on my skills and abilities</p>
@@ -31,11 +31,11 @@ const Information = () => {
                 <div className='mt-16 md:mt-28 px-6 md:w-2/3 flex flex-row justify-between'>
                     <div className=''>
                         <h1 className='font-bold text-5xl md:text-8xl text-[#4C89F8]'>2 +</h1>
-                        <p className='text-xl md:text-3xl '>Projects Done</p>
+                        <p className='text-lg md:text-3xl '>Projects Done</p>
                     </div>
                     <div className=''>
                         <h1 className='font-bold text-5xl md:text-8xl text-[#4C89F8]'>7 +</h1>
-                        <p className='text-xl md:text-3xl '>Skills</p>
+                        <p className='text-lg md:text-3xl '>Skills</p>
                     </div>
 
                 </div>
@@ -44,15 +44,15 @@ const Information = () => {
             {/* ----------------------------Educations ---------------------------------------------------------------- */}
             <div className='mt-16'>
                 <div className='mt-20'>
-                    <h1 className='lg:text-2xl py-1'>Education</h1>
+                    <h1 className='lg:text-2xl text-lg py-1'>Education</h1>
                     <hr />
                 </div>
-                <div className='mt-16 space-y-12'>
+                <div className='mt-10 md:mt-16 space-y-12'>
                     <Education institute="ST. ANTHONY'S HIGH SCHOOL" std="SSC" period="2018-19" board="Maharashtra state board" status="Passed" />
                     <Education institute="Viva College" std="hsc" period="2020-21" board="Maharashtra state board" status="Passed" />
                     <Education institute="Vartak College" std="BSC CS" period="2022-23" board="Mumbai University" status="Passed" />
-                    <p className='text-2xl font-base'>
-                        "<span className='text-3xl font-semibold text-[#4C89F8]'>Education is not a destination</span>; it is a lifelong journey fueled by curiosity.
+                    <p className='text-md md:text-2xl font-base'>
+                        "<span className='text-xl md:text-3xl font-semibold text-[#4C89F8]'>Education is not a destination</span>; it is a lifelong journey fueled by curiosity.
                         As I tread the path of learning, I embrace the boundless horizons of knowledge,
                         for I am a seeker who will never cease to acquire wisdom and forge a future
                         illuminated by the brilliance of continuous growth."
@@ -62,11 +62,11 @@ const Information = () => {
             {/* ---------------------------------Skills ------------------------------------------------------------------- */}
             <div className='mt-16 space-y-12'>
                 <div className='mt-20'>
-                    <h1 className='lg:text-2xl py-1'>Skills</h1>
+                    <h1 className='lg:text-2xl text-lg py-1'>Skills</h1>
                     <hr />
                 </div>
-                <div className='space-y-20 py-4'>
-                    <div className='grid grid-cols-3 gap-16'>
+                <div className='md:space-y-20 py-4'>
+                    <div className='flex flex-wrap'>
                         <ProgressBar value={85} skill="Photoshop" />
                         <ProgressBar value={78} skill="Illustrator" />
                         <ProgressBar value={90} skill="Adobe XD" />
@@ -74,18 +74,18 @@ const Information = () => {
                         <ProgressBar value={75} skill="React Js" />
                         <ProgressBar value={53} skill="Next-Js" />
                     </div>
-                    <h1 className='text-2xl font-base'>"From a noob to a pro, my learning path is an evolving symphony of skills.
+                    <h1 className='text-md md:text-2xl font-base mt-6 md:mt-0'>"From a noob to a pro, my learning path is an evolving symphony of skills.
                         With relentless dedication, I forge ahead, embracing the challenges that
                         shape me into a masterful developer."</h1>
                 </div>
             </div>
-            {/* ---------------------------------Skills ------------------------------------------------------------------- */}
+            {/* ---------------------------------experience ------------------------------------------------------------------- */}
             <div className='mt-16 space-y-12'>
                 <div className='mt-20'>
-                    <h1 className='lg:text-2xl py-1'>Experience</h1>
+                    <h1 className='lg:text-2xl text-lg py-1'>Experience</h1>
                     <hr />
                 </div>
-                <div className='space-y-20 py-4'>
+                <div className='space-y-10 md:space-y-20 py-4'>
                     <Experience year={'2021'} job={'DATA ENTRY JOB'} period={"6 months"} company={"Riddhi coporation/Mumbai"} des={`"I manage data, ensuring everything is neatly arranged and easily accessible, paving the way for smooth and efficient operations."`} />
                     <Experience year={'2022'} job={'GRAPHIC DESIGNER '} period={"3 months"} company={"AdFJ studios/Boisar"} des={`"In the realm of design, I masterfully create captivating logos, banners, brochures, social media posts, and videos. With each stroke and edit, I breathe life into visuals that leave a lasting impression."`} />
                     <Experience year={'2023'} period={'Currently working'} job={'Developer / graphic designer'}  company={"Sai Ashirwad Informatia/Bhayander"} des={`"I paint the digital canvas with pixels, blend code and creativity, sculpting experiences that bridge art and technology. As a multidisciplinary creator, I bring visions to life, weaving design, development, and user-centered magic into seamless digital tapestries."`} />
